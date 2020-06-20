079c6a5c1594081dea1e33ac362adb8f7e446396
hariprasadsh <63358097+hariprasadsh@users.noreply.github.com>
2020-06-21 02:51:38 +0530

    added functions to language controller

---
+++ src/controllers/language.controller.js
@@ -0,0 +1,17 @@
+const Language = require("../models/language.model");
+const { findAll } = require("./genere.controller");
+
+// find all languages
+exports.findAll = (req, res) => {};
+
+// create a language
+exports.create = (req, res) => {};
+
+// find a language by id
+exports.findById = (req, res) => {};
+
+// update a language
+exports.update = (req, res) => {};
+
+// delete a language
+exports.delete = (req, res) => {};
